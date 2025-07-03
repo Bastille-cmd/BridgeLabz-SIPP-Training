ff9cb4aadc6fdb0cb6f9dafabbfa7a018484eed1
Chetan Chaudhary <chetanchaudhary.18804@gmail.com>
2025-07-03 09:13:27 +0530

    [Chetan Chaudhary]-inheritance

---
+++ inheritance/HierarchicalInheritance_BankAccount.java
@@ -0,0 +1,48 @@
+class BankAccount {
+    String accountNumber;
+    double balance;
+
+    BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    double interestRate;
+
+    SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    void displayAccountType() {
+        System.out.println("Savings Account");
+    }
+}
+
+class CheckingAccount extends BankAccount {
+    double withdrawalLimit;
+
+    CheckingAccount(String accountNumber, double balance, double withdrawalLimit) {
+        super(accountNumber, balance);
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    void displayAccountType() {
+        System.out.println("Checking Account");
+    }
+}
+
+class FixedDepositAccount extends BankAccount {
+    int termLength;
+
+    FixedDepositAccount(String accountNumber, double balance, int termLength) {
+        super(accountNumber, balance);
+        this.termLength = termLength;
+    }
+
+    void displayAccountType() {
+        System.out.println("Fixed Deposit Account");
+    }
+}
